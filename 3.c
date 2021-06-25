40e026944e86ed88834a5b3939288d0b1d975355
SHEILA506 <85747624+SHEILA506@users.noreply.github.com>
2021-06-25 13:57:08 +0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,14 @@
+//program to calculate area of a circle
+#include<stdio.h>;
+int main()
+{
+int radius,area;
+
+printf("radius of the circle");
+
+area=3.14*radius*radius;
+printf("area of the circle");
+
+	
+	return 0;
+}
